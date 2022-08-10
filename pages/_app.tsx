afe224399be052fe697bf0eacd2cc5fd11b5e46e
Vitaliy Zavgorodniy <vince.kkr@gmail.com>
2022-08-11 01:22:39 +0300

    removed redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,17 @@
-import type { AppProps } from "next/app";
-import { Provider } from "react-redux";
-import { ThemeProvider } from "styled-components";
+import type { AppProps } from 'next/app';
+// import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
 
-import store from "redux/store";
-import { lightTheme } from "constants/theme";
+// import store from 'redux/store';
+import { lightTheme } from 'constants/theme';
 
-import "styles/globals.css";
-import "styles/fonts.css";
+import 'styles/globals.css';
+import 'styles/fonts.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Provider store={store}>
-    <ThemeProvider theme={lightTheme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  </Provider>
+  <ThemeProvider theme={lightTheme}>
+    <Component {...pageProps} />
+  </ThemeProvider>
 );
 
 export default MyApp;
